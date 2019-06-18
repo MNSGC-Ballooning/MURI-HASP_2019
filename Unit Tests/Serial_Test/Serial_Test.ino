8738c8b08370a4ccef9975b02f80b690bd856136
Jacob Meiners <meine042@umn.edu>
2019-06-18 09:39:38 -0500

    removed old serial test and replaced with up to date one

---
+++ Unit Tests/Serial_Test/Serial_Test.ino
@@ -0,0 +1,66 @@
+/*
+ * HASP Serial Communicationm Unit test.
+ * 
+ * This code should be run on a seperate microcontroller (Arduino) and then be connected
+ * via serial connection to the hasp payload.
+ * 
+ * This will simulate reading data and sending commands to the HASP payload.
+ */
+
+
+// define commands
+#define C1 0xAA
+#define C2 0xBB
+#define C3 0xCC
+#define CS 0x1C
+
+int i = 1;
+int Idata [54]; // 54 bytes of data (int)
+char Cdata [54]; // 54 bytes of data (char)
+
+void setup() 
+{
+  Serial.begin(1200); // initialize serial connection to HASP baud rate
+  delay(100);
+}
+
+void loop() 
+{
+  // read downlink data
+  int k = 0;
+  while(Serial.available() > 0 && k<54) // may need to make this stop after the 54 bytes have been read but idk
+  {
+    Idata[k] = Serial.read();
+    k++;
+  }
+  // print downlink data to serial monitor
+  for(int j=0;j<54;j++) // 54 bytes of data
+  {
+    Cdata[j] = Idata[j]; // convert int data to char 
+                         // might be redundant, I can probably just say "Cdata = Serial.read();"
+    Serial.print(Cdata[j]); 
+  }
+  Serial.println();
+  
+  // send 1 of 3 commands 
+  if(i==1)
+  {
+    Serial.write(CS); // send checksum and command 1
+    Serial.write(C1);
+    i++;
+  }
+  else if(i==2)
+  {
+    Serial.write(CS); // send checksum and command 2
+    Serial.write(C2);
+    i++;  
+  }
+  else if(i==3)
+  {
+    Serial.write(CS); // send checksum and command 3
+    Serial.write(C3);
+    i=0;  
+  }
+  delay(500);
+  
+}
